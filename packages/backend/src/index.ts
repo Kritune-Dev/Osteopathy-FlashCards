0d2b4b1085f7e12a44e7efa9cec382a025adf090
Corentin Bacconnais <corentin.bacconnais@gmail.com>
2022-03-16 16:24:43 +0100

    ➕ : Add base project node ts

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,7 +1,24 @@
 'use strict';
 
-module.exports = backend;
+//import express and body-parser
+const express = require('express');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+require('dotenv').config()
 
-function backend() {
-    console.log("Hello from backend");
-}
+//create express app
+const app = express();
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+// secure apps by setting various HTTP headers
+app.use(helmet());
+
+// listen for requests and connect to mangoDB
+app.listen(process.env.PORT, () => {
+    console.log('Server is listening on port 3000');
+});
